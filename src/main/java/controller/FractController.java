6eeabd06d17fddd4d1e391da53109e3a6f7e1ee0
LE FRANC matthieu <matt.lefranc@gmail.com>
2021-12-21 14:36:35 +0100

    image dans l'interface

--- src/main/java/controller/FractController.java
+++ src/main/java/controller/FractController.java
@@ -2,18 +2,26 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.stage.Stage;
 import model.Complex;
 import model.Fractal;
 import model.FractalBuilder;
 import model.FractalDefinitionDomain;
 import model.FractalDesigner;
-import view.ViewManager;
 
 public class FractController implements Initializable {
     
@@ -33,6 +41,8 @@ public class FractController implements Initializable {
     private TextField gap;
     @FXML
     private Button createFract;
+    @FXML
+    private ImageView fractalView;
 
     public FractController(String real, String imaginary) {
 
@@ -47,12 +57,32 @@ public class FractController implements Initializable {
         Fractal f = new FractalBuilder().setDefinitionDomain(new FractalDefinitionDomain(Double.parseDouble(dimX1.getText()), Double.parseDouble(dimX2.getText()), 
             Double.parseDouble(dimY1.getText()), Double.parseDouble(dimY2.getText()))).setGap(Double.parseDouble(gap.getText())).setComplexConstant(new Complex(Double.parseDouble(real.getText()), Double.parseDouble(imaginary.getText()))).buildJulia();
         FractalDesigner leDesigner = new FractalDesigner(f);
-        leDesigner.writeImage(leDesigner.drawFractal());
+        BufferedImage img = leDesigner.drawFractal();
+        leDesigner.writeImage(img);
+        Image image = convertToFxImage(img);
+        fractalView.setImage(image);
+        /* Stage thisStage = (Stage) gap.getScene().getWindow();
+        thisStage.show(); */ 
     }
+
+    private static Image convertToFxImage(BufferedImage image) {
+        WritableImage wr = null;
+        if (image != null) {
+            wr = new WritableImage(image.getWidth(), image.getHeight());
+            PixelWriter pw = wr.getPixelWriter();
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    pw.setArgb(x, y, image.getRGB(x, y));
+                }
+            }
+        }
+    
+        return new ImageView(wr).getImage();
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
-        
+
     }
 
 }
