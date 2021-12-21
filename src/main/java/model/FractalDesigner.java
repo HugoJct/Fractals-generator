6f70afde9675aa3839be05081501d6872c2565f8
JACOTOT Hugo <hjacotot@free.fr>
2021-12-21 12:29:38 +0100

    Modified the designer to build mandelbrot when asked

--- src/main/java/model/FractalDesigner.java
+++ src/main/java/model/FractalDesigner.java
@@ -16,66 +16,76 @@ public class FractalDesigner {
 
 	public FractalDesigner(Fractal f) {
 		this.fr = f;
-		this.f = new File(f.getComplexConstant().getRealPart() + "_" + f.getComplexConstant().getImaginaryPart() + ".png");
+		this.f = new File(
+				f.getComplexConstant().getRealPart() + "_" + f.getComplexConstant().getImaginaryPart() + ".png");
 	}
-	
+
 	private double standardDev(double a, double b) {
 		if (a < 0 && b < 0) {
-			return b<a?Math.abs(a)-Math.abs(b):Math.abs(b)-Math.abs(a);
-		}
-		else if (a>0 && b<0) {
-			return a+Math.abs(b);
-		}
-		else if (a<0 && b>0) {
-			return Math.abs(a)+b;
-		}
-		else {
-			return b<a?a-b:b-a;
+			return b < a ? Math.abs(a) - Math.abs(b) : Math.abs(b) - Math.abs(a);
+		} else if (a > 0 && b < 0) {
+			return a + Math.abs(b);
+		} else if (a < 0 && b > 0) {
+			return Math.abs(a) + b;
+		} else {
+			return b < a ? a - b : b - a;
 		}
 	}
-	
+
 	public BufferedImage drawFractal() {
-		double imagex=(fr.getDomain().getMax().getRealPart() - fr.getDomain().getMin().getRealPart()) / fr.getGap();
-		double imagey=(fr.getDomain().getMax().getImaginaryPart() - fr.getDomain().getMin().getImaginaryPart()) / fr.getGap();
-		BufferedImage img = new BufferedImage((int)Math.round(imagex), (int)Math.round(imagey), BufferedImage.TYPE_INT_RGB);
-		
+		double imagex = (fr.getDomain().getMax().getRealPart() - fr.getDomain().getMin().getRealPart()) / fr.getGap();
+		double imagey = (fr.getDomain().getMax().getImaginaryPart() - fr.getDomain().getMin().getImaginaryPart())
+				/ fr.getGap();
+		BufferedImage img = new BufferedImage((int) Math.round(imagex), (int) Math.round(imagey),
+				BufferedImage.TYPE_INT_RGB);
+
 		int nbThreads = 2;
 		int portionSize = (int) imagey / nbThreads;
-		
+
 		double startY = fr.getDomain().getMin().getImaginaryPart();
-		double portionY = standardDev(startY, fr.getDomain().getMax().getImaginaryPart())/nbThreads;
+		double portionY = standardDev(startY, fr.getDomain().getMax().getImaginaryPart()) / nbThreads;
 		double endY = startY + portionY;
-		
+
 		long startTime = System.nanoTime();
-		
-		for(int i=0;i<nbThreads;i++) {
-			
-			FractalDefinitionDomain fdd = new FractalDefinitionDomain(fr.getDomain().getMin().getRealPart(), fr.getDomain().getMax().getRealPart(), startY, endY);
-			Fractal tmp = new FractalBuilder().setComplexConstant(fr.getComplexConstant())
-					.setGap(fr.getGap()).setDefinitionDomain(fdd).buildJulia();
+
+		for (int i = 0; i < nbThreads; i++) {
+
+			FractalDefinitionDomain fdd = new FractalDefinitionDomain(fr.getDomain().getMin().getRealPart(),
+					fr.getDomain().getMax().getRealPart(), startY, endY);
+			Fractal tmp;
+			if (fr instanceof JuliaFractal)
+				tmp = new FractalBuilder().setComplexConstant(fr.getComplexConstant()).setGap(fr.getGap())
+						.setDefinitionDomain(fdd).buildJulia();
+			else
+				tmp = new FractalBuilder().setComplexConstant(fr.getComplexConstant()).setGap(fr.getGap())
+						.setDefinitionDomain(fdd).buildMandelbrot();
 			FractalThread t = new FractalThread(tmp, img, i, portionSize);
 			t.start();
 			tList.add(t);
 			startY += portionY;
 			endY += portionY;
 		}
-		
-		for(Thread t : tList) {
+
+		for (Thread t : tList) {
 			try {
 				t.join();
-			} catch (InterruptedException e) {e.printStackTrace();}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-		
+
 		long stopTime = System.nanoTime();
-		System.out.println("Runtime: "+((stopTime-startTime)/1000000)+"ms");
-		
+		System.out.println("Runtime: " + ((stopTime - startTime) / 1000000) + "ms");
+
 		return img;
 	}
-	
+
 	public void writeImage(BufferedImage img) {
 		try {
 			ImageIO.write(img, "PNG", f);
-		} catch (IOException e) {e.printStackTrace();}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
